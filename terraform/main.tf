fb166c15cfe412317a7969498e2fd001bda9826a
RajsJohal <rajsjohal97@hotmail.com>
2022-01-14 12:48:18 +0000

    update sg rules for db

--- terraform/main.tf
+++ terraform/main.tf
@@ -151,6 +151,14 @@ resource "aws_security_group" "db_sg" {
     cidr_blocks = ["10.0.7.0/24"]
   }
 
+  ingress {
+    description = "port 22"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port = 0
     to_port = 0
