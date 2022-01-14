44df27419a218e09f587c6ee926406da523a11a6
RajsJohal <rajsjohal97@hotmail.com>
2022-01-14 15:57:08 +0000

    created another hosts.inv file

--- terraform/hosts_config.sh
+++ terraform/hosts_config.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo -e "[app]\nec2-instance ansible_host=$(terraform output app_instance_ip) ansible_user=ubuntu" | sed 's/"//g' > hosts.inv
+echo -e "[app]\nec2-instance ansible_host=$(terraform output app_instance_ip) ansible_user=ubuntu" | sed 's/"//g' > hosts_app.inv
 
-echo -e "[db]\nec2-instance ansible_host=$(terraform output db_instance_ip) ansible_user=ubuntu" | sed 's/"//g' >> hosts.inv
+echo -e "[db]\nec2-instance ansible_host=$(terraform output db_instance_ip) ansible_user=ubuntu" | sed 's/"//g' > hosts_db.inv
